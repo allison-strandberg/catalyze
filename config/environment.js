33bc4eae3bd6a0efa72e017b292c3e4625d9e415
allison-strandberg <allisonstrandberg@gmail.com>
2020-07-21 23:17:07 -0400

    Add nav buttons

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,12 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    googleFonts: [
+      'Open+Sans:300,400,700',
+      'Press+Start+2P:300',
+    ]
   };
 
   if (environment === 'development') {
