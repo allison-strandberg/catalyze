91179b4a937043b7c110dea4e2dab987efbafc3e
allison-strandberg <allisonstrandberg@gmail.com>
2020-10-18 13:32:45 -0400

    Add burger menu items

--- app/components/burger-menu.js
+++ app/components/burger-menu.js
@@ -1,6 +1,12 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class BurgerMenuComponent extends Component {
   @tracked open = false;
+
+  @action
+  toggleMenu() {
+    this.open = !this.open;
+  }
 }
