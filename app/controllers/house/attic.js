0570feb8a353701934ad6e3abb01ec8bca84c6b9
allison-strandberg <allisonstrandberg@gmail.com>
2020-10-24 15:00:40 -0400

    Add Wyoming images

--- app/controllers/house/attic.js
+++ app/controllers/house/attic.js
@@ -3,17 +3,28 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class HouseAtticController extends Controller {
-  queryParams = ['magnifying_glass', 'bookshelf'];
+  queryParams = [
+    'magnifying_glass',
+    'bookshelf',
+    'northeast_window',
+    'northwest_window',
+  ];
 
   @tracked magnifying_glass;
 
   @tracked bookshelf;
+
+  @tracked northeast_window;
+
+  @tracked northwest_window;
 
   @action
   toAttic() {
     this.transitionToRoute('house.attic', { queryParams: { 
       magnifying_glass: null,
       bookshelf: null,
+      northeast_window: null,
+      northwest_window: null,
     }});
   }
 }
