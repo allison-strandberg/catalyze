d713bf8334a5bc39819e577f15458b9cf399c289
allison-strandberg <allisonstrandberg@gmail.com>
2020-10-23 23:44:08 -0400

    Add begonia documents

--- app/controllers/house/first-floor.js
+++ app/controllers/house/first-floor.js
@@ -3,12 +3,24 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class HouseFirstFloorController extends Controller {
-  queryParams = ['begonias'];
+  queryParams = ['begonias', 'lemongrass', 'snake_plant', 'pothos', 'begonia_sketch'];
 
   @tracked begonias;
 
+  @tracked lemongrass;
+
+  @tracked snake_plant;
+
+  @tracked pothos;
+
   @action
   toFirstFloor() {
-    this.transitionToRoute('house.first-floor', { queryParams: { begonias: null } });
+    this.transitionToRoute('house.first-floor', { queryParams: { 
+      begonias: null,
+      lemongrass: null,
+      snake_plant: null,
+      pothos: null,
+      begonia_sketch: null,
+    }});
   }
 }
