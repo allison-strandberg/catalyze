935f51e7ec21e4f54ff8f464e24980bb41e766d7
allison-strandberg <allisonstrandberg@gmail.com>
2020-10-24 14:16:57 -0400

    Add bookshelf image

--- app/controllers/house/attic.js
+++ app/controllers/house/attic.js
@@ -3,14 +3,17 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class HouseAtticController extends Controller {
-  queryParams = ['magnifying_glass'];
+  queryParams = ['magnifying_glass', 'bookshelf'];
 
   @tracked magnifying_glass;
+
+  @tracked bookshelf;
 
   @action
   toAttic() {
     this.transitionToRoute('house.attic', { queryParams: { 
       magnifying_glass: null,
+      bookshelf: null,
     }});
   }
 }
