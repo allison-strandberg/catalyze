258b74c84834a6e4fc4ced71ff8d695e7c3c4c83
allison-strandberg <allisonstrandberg@gmail.com>
2020-10-24 17:25:06 -0400

    Remove demo query param

--- app/controllers/house.js
+++ app/controllers/house.js
@@ -1,8 +1,4 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 
 export default class HouseController extends Controller {
-  queryParams = ['demo'];
-
-  @tracked demo = false;
 }
