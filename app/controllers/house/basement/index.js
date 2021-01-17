5c914b78cf31e6bc7d44a9407bb181e36d0b0131
allison-strandberg <allisonstrandberg@gmail.com>
2021-01-16 23:39:40 -0500

    Add diary entry

--- app/controllers/house/basement/index.js
+++ app/controllers/house/basement/index.js
@@ -5,14 +5,18 @@ import { action } from '@ember/object';
 export default class HouseBasementIndexController extends Controller {
   queryParams = [
     'clipboard',
+    'diary',
   ];
 
   @tracked clipboard;
+
+  @tracked diary;
 
   @action
   toBasement() {
     this.transitionToRoute('house.basement.index', { queryParams: { 
       clipboard: null,
+      diary: null,
     }});
   }
 }
