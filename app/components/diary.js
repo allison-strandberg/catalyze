5a35bf675646293c2e8210c175382140caf082f6
allison-strandberg <allisonstrandberg@gmail.com>
2021-01-17 00:04:25 -0500

    Add more diary entries

--- app/components/diary.js
+++ app/components/diary.js
@@ -1,4 +1,35 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class DiaryComponent extends Component {
+  @tracked page = 0;
+
+  totalPages = 4;
+
+  get first() {
+    return this.page === 0;
+  }
+
+  get second() {
+    return this.page === 1;
+  }
+
+  get third() {
+    return this.page === 2;
+  }
+
+  get fourth() {
+    return this.page === 3;
+  }
+
+  @action
+  nextPage() {
+    this.page = (this.page + 1) % this.totalPages;
+  }
+
+  @action
+  previousPage() {
+    this.page = this.page ? this.page - 1 : this.totalPages - 1;
+  }
 }
